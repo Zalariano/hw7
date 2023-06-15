5e6d34ca4716c8436cb97fc05ef58fd06b9c2522
Romchig <romulusto@gmail.com>
2023-06-15 20:38:25 +0200

    Исправления

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,90 @@
-public class Main { public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
 
-    // Задание номер 1
-    int salary = 15000;
-    int total = 0;
-    int m = 0;
-    while (total < 2_459_000) {
-        total = total + total/100;
-        total = total + salary;
-        m = m + 1;
-        System.out.println("Месяц " + m + ", сумма накоплений равна " + total + " рублей");}
-
-    //Задание номер  2
-    int i = 0;
-    while (i < 10) {
-        i = i + 1;
-        System.out.print(i + " ");}
-    System.out.println();
-    for (i = 10; i >=1; i = i-1) {System.out.print(i + " ");}
-    System.out.println();
-
-    //Задание номер 3
-    int people = 12_000_000;
-    int rody = people/1_000 * 17;
-    int smerty = people/1_000 * 8;
-    int raznica = rody - smerty;
-    for (int g = 1; g <= 10; g = g + 1) {
-        people = people + raznica;
-        rody = people/1_000 * 17;
-        smerty = people/1_000 * 8;
-        raznica = rody - smerty;
-        System.out.println("Год " + g + ", численность населения составляет" + people );}
-
-    //Задание номер 4
-    int vklad = 15000;
-    int summa = 0;
-    int meciac = 0;
-    while (summa < 12_000_000) {
-        summa = summa + ((summa/100) * 7);
-        summa = summa + vklad;
-        meciac = meciac + 1;
-        System.out.println("Месяц " + meciac + ", сумма накоплений равна " + summa + " рублей");}
-
-    //Звдвние номер 5
-    int vklad1 = 15000;
-    int summa1 = 0;
-    int meciac1 = -1;
-    while (summa1 < 12_000_000) {
-        summa1 = summa1 + ((summa1 / 100) * 7);
-        summa1 = summa1 + vklad1;
-        meciac1 = meciac1 + 1;
-        if (meciac1 % 6 == 0) {
-            System.out.println("Месяц " + meciac1 + ", сумма накоплений равна " + summa1 + " рублей");}
-    }
-
-    //Задание номер 6
-    int vklad2 = 15000;
-    int summa2 = 0;
-    int allTime = 9;
-    int allTimeMeciac = allTime * 12;
-    for (int m1 = 0; m1 <= allTimeMeciac; m1 = m1 + 1) {
-        summa2 = summa2 + ((summa2 / 100) * 7);
-        summa2 = summa2 + vklad2;
-        if (m1 % 6 == 0) {
-            System.out.println("Месяц " + m1 + ", сумма накоплений равна " + summa2 + " рублей");
+        // Задание номер 1
+        int salary = 15000;
+        int total = 0;
+        int month = 0;
+        int itog = 2459000;
+        while (total < itog) {
+            total = total + salary;
+            month = month + 1;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
         }
-    }
 
-    //Задание номер 7
-    int pervaia = 3;
-    System.out.println("Сегодня пятница, " + pervaia+ " -е число. Необходимо подготовить отчет");
-    while (pervaia <= 24) {
-        pervaia = pervaia + 7;
-        System.out.println("Сегодня пятница, " + pervaia+ " -е число. Необходимо подготовить отчет");}
+        //Задание номер  2
+        int i = 0;
+        while (i < 10) {
+            i = i + 1;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (i = 10; i >= 1; i = i - 1) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
 
-    //Задание номер 8
-    int god = 2023;
-    for (int years = god - 200 ; years <= god + 100; years++) {
-        if (years % 79 == 0) {
-            System.out.println(years);}
-    }
+        //Задание номер 3
+        int people = 12_000_000;
+        int rodyNaTysiaczy = 17;
+        int smertyNaTysiaczy = 8;
+        for (int god = 1; god <= 10; god = god + 1) {
+            int rody = people / 1_000 * rodyNaTysiaczy;
+            int smerty = people / 1_000 * smertyNaTysiaczy;
+            int raznica = rody - smerty;
+            people = people + raznica;
+            System.out.println("Год " + god + ", численность населения составляет" + people);
+        }
+
+        //Задание номер 4
+        int vklad = 15000;
+        int meciac = 0;
+        int procent = 7;
+        int itog1 = 12000000;
+        while (vklad < itog1) {
+            vklad = vklad + (vklad / 100 * procent);
+            meciac = meciac + 1;
+            System.out.println("Месяц " + meciac + ", сумма накоплений равна " + vklad + " рублей");
+        }
+
+        //Звдвние номер 5
+        int vklad1 = 15000;
+        int meciac1 = -1;
+        int konecznajaSumma = 12000000;
+        while (vklad1 < konecznajaSumma) {
+            vklad1 = vklad1 + (vklad1 / 100 * procent);
+            meciac1 = meciac1 + 1;
+            if (meciac1 % 6 == 0) {
+                System.out.println("Месяц " + meciac1 + ", сумма накоплений равна " + vklad1 + " рублей");
+            }
+        }
+
+        //Задание номер 6
+        int vklad2 = 15000;
+        int allTime = 9;
+        int allTimeMeciac = allTime * 12;
+        for (int month1 = 0; month1 <= allTimeMeciac; month1 = month1 + 1) {
+            vklad2 = vklad2 + ((vklad2 / 100) * procent);
+            if (month1 % 6 == 0) {
+                System.out.println("Месяц " + month1 + ", сумма накоплений равна " + vklad2 + " рублей");
+            }
+        }
+
+        //Задание номер 7
+        int pervaia = 2;
+        int dneyVMecyace = 31;
+        while (pervaia <= dneyVMecyace) {
+            System.out.println("Сегодня пятница, " + pervaia + " -е число. Необходимо подготовить отчет");
+            pervaia = pervaia + 7;
+
+        }
+
+        //Задание номер 8
+        int god = 2023;
+        for (int years = god - 200; years <= god + 100; years++) {
+            if (years % 79 == 0) {
+                System.out.println(years);
+            }
+        }
     }
 }
